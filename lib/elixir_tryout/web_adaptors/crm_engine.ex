635e69290966f443a2cdb40679d441fc6a959577
Ivan Chervenets <chervenets.ivan@gmail.com>
2018-12-27 15:39:23 +0200

    TCC-15787 | Fixed http adaptors.

--- lib/elixir_tryout/web_adaptors/crm_engine.ex
+++ lib/elixir_tryout/web_adaptors/crm_engine.ex
@@ -1,7 +1,15 @@
 defmodule ElixirTryout.WebAdaptors.CrmEngine do
   import ElixirTryout.WebAdaptors.BaseAdaptor
 
-  def find_accounts(params \\ []) do
-    get("crm/v2/accounts/account/find", params)
+  def find_accounts(params \\ %{}) do
+    get(host(), "crm/v2/accounts/account/find", params)
+  end
+
+  def create_virtual_account(params \\ %{}) do
+    post(host(), "crm/virtual_accounts/internal/create", params)
+  end
+
+  defp host do
+    "http://localhost:38081/"
   end
 end
