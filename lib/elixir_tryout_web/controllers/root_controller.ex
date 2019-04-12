7d6c1f91da6ebec896a419d7ae5ffdf238aa7252
Borys Bartsyts <bartzitz@gmail.com>
2019-04-13 00:23:39 +0300

    Added more elemets to mockup

--- lib/elixir_tryout_web/controllers/root_controller.ex
+++ lib/elixir_tryout_web/controllers/root_controller.ex
@@ -6,6 +6,48 @@ defmodule ElixirTryoutWeb.RootController do
   end
 
   def index_html(conn, _params) do
-    render(conn, "index.html")
+    transactions = [
+      %{
+        client: "Azimo ltd",
+        client_enabled: true,
+        sender: "Random guy",
+        sender_status: "Unknown",
+        sender_css: "badge-secondary",
+        account: "Arkea payment",
+        amount: "1005000.00",
+        currency: "EUR",
+        value_date: "11 Apr 19",
+        created_on: "11 Apr 19"
+      },
+      %{
+        client: "Another client",
+        client_enabled: false,
+        sender: "Other guy",
+        sender_status: "Unknown",
+        sender_css: "badge-secondary",
+        account: "CFSB client",
+        amount: "300.00",
+        currency: "USD",
+        value_date: "11 Apr 19",
+        created_on: "11 Apr 19"
+      }
+    ]
+
+    item = %{
+      client: "Company x",
+      client_enabled: true,
+      sender: "Proper company",
+      sender_status: "Approved",
+      sender_css: "badge-success",
+      account: "Barclays client",
+      amount: "3200.00",
+      currency: "GBP",
+      value_date: "11 Apr 19",
+      created_on: "11 Apr 19"
+    }
+
+    transactions = (1..10) |> Enum.map(fn _ -> item end) |> Enum.into(transactions)
+
+    render(conn, "index.html", transactions: transactions)
   end
 end
--- lib/elixir_tryout_web/views/root_view.ex
+++ lib/elixir_tryout_web/views/root_view.ex
@@ -1,3 +1,11 @@
 defmodule ElixirTryoutWeb.RootView do
   use ElixirTryoutWeb, :view
+
+  def approval_disabled(row) do
+    if row.client_enabled do
+      false
+    else
+      true
+    end
+  end
 end
